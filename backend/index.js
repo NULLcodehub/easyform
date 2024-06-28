e8803457ac53945a4ea286ac9a40d70fb0e319e3
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-06-28 22:16:46 +0600

    server initialized

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,23 @@
-const express=
+require("dotenv").config()
+const express=require("express")
+const bodyParser=require('body-parser')
+const cors=require('cors')
+
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const PORT= process.env.PORT
+
+
+app.get('/',(req,res)=>{
+    res.send('server working')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
+
+
