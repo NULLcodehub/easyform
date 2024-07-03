500cda69fe23efd2999fde07399ea4f8106abb06
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-03 14:08:41 +0600

    project routes implement done  and project conneted to database

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,16 +1,43 @@
 const shortID=require('short-uuid')
 
+const Project=require('../models/projectModel')
 
 
 const projectController=async (req,res)=>{
     
-    const {projectname}=req.body;
+    const {projectname,user_id}=req.body;
 
-    const pass_key=shortID.generate()
+    
 
-    res.send(pass_key)
+    try{
+        
+
+        if(!projectname){
+            res.send("No project name")
+        }else{
+            const form_api_key=shortID.generate()
+            const projectOk=new Project({projectname:projectname,form_api_key:form_api_key,user_id:user_id,create_at:Date.now()})
+            
+            if(!projectOk){
+                res.send('somthing is wrong,try again')
+                
+            }
+            else{
+                await projectOk.save()
+                res.send(projectOk)
+            }
+        }
 
 
+
+    }catch(err){
+        res.send(err)
+    }
+
+
+
+    
+    
 }
 
 module.exports=projectController
--- backend/models/projectModel.js
+++ backend/models/projectModel.js
@@ -4,10 +4,10 @@ const Schema=mongoose.Schema
 const projectSchema=new Schema({
     user_id:{type:mongoose.Schema.Types.ObjectId,ref:'User'},
     projectname:{type:String,required:true},
-    pass_key:{type:String,unique:true,required:true},
+    form_api_key:{type:String,unique:true,required:true},
     create_at:{type:Date,default:Date.now}
 })
 
-const Project=mongoose.model('User',projectSchema)
+const Project=mongoose.model('Project',projectSchema)
 
 module.exports=Project
