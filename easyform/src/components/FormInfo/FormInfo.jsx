8825c4818502b8d0054d872e2c4f603438178d67
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-26 16:33:33 +0600

    code example of react form added and scroll bar edited

--- easyform/src/components/FormInfo/FormInfo.jsx
+++ easyform/src/components/FormInfo/FormInfo.jsx
@@ -51,6 +51,80 @@ const FormInfo =({loadProjectData}) => {
 
 
 
+
+    const codeString=
+    `// src/components/FormComponent.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const FormComponent = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('https://api.example.com/submit', formData);// repleace  api with your FormFlow api end point
+      console.log('Form submitted successfully:', response.data);
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default FormComponent;
+
+    `
+
     
     return (
         <>
@@ -90,7 +164,7 @@ const FormInfo =({loadProjectData}) => {
                         <section className=' h-80vh codeExampleMain'>
                             <div className='bg-white mx-5 my-5 h-65vh rounded-lg p-7'>
                                 <SyntaxHighlighter language='javascript' style={okaidia} className='h-full'>
-                                    
+                                    {codeString}
                                 </SyntaxHighlighter>
                             </div>
                         </section>
