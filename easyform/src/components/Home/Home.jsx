9c867e575c3e8720086be03e973c0d0d5d448ebe
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-30 13:39:10 +0600

    code snepit image added to the landing page

--- easyform/src/components/Home/Home.jsx
+++ easyform/src/components/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import codeImage from "../../../public/assets/images/landingImage.png"
 const Home = () => {
     return (
-        <div>
-            hello form home
-        </div>
+        <>
+            <main className='mx-8 md:mx-20'>
+
+                <section>
+                    <div className=' mt-20'>
+                        <center>
+                            <h1 className='text-30px md:text-40px font-extrabold'>Receive form submissions directly to your email.</h1>
+                            <p className='mt-3 md:text-xl text-gray-600'>Write your form code,connect to our api, and we'll handle the process</p>
+                            <div className='my-5 rounded-md'>
+                                <img src={codeImage} alt="code-snepit" className='rounded-xl'  loading='lazy' />
+                            </div>
+                            
+                        </center>
+                    </div>
+                    
+                </section>
+
+            </main>
+        
+        </>
+
     );
-};
+};  
 
 export default Home;
