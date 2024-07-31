3b584a3d04414164a10a48b914a928e4cb9ff4f4
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-31 22:18:20 +0600

    registration page added

--- easyform/src/components/Home/Home.jsx
+++ easyform/src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import codeImage from "../../../public/assets/images/landingImage.png"
+import addnew from '../../assets/add new.png'
+import apiendpintcode from '../../assets/apiendpoint.png'
 const Home = () => {
     return (
         <>
@@ -28,11 +30,21 @@ const Home = () => {
 
                     <div className='my-10'>
                         <center >
-                            <div className='md:w-7/12  text-left'>
+                            <div className='md:w-7/12 text-gray-600  text-left'>
                                 <h1 className='text-xl md:text-2xl font-semibold my-5'>1. Create an account </h1>
                                 <h1 className='text-xl md:text-2xl font-semibold my-5'>2. Click on "Add new project" & Create a project.You will get an api end point.  </h1>
+                                <div className='my-6'>
+                                    <img src={addnew} className='border-2 md:mx-4' alt="" />
+                                </div>
                                 <h1 className='text-xl md:text-2xl font-semibold my-5'>3. Copy and paste the api in your react form </h1>
+                                <div className='my-3'>
+                                    <img src={apiendpintcode} alt="" loading='lazy' className='md:w-9/12 md:mx-4 rounded-md' />
+                                </div>
+                                <h1 className='text-xl md:text-2xl font-semibold my-5'>4. Now your work is done. Rest is on us. Massages will we send to your Email Adress directly  </h1>
+
                             </div>
+
+                           
                             
                         </center>
                     </div>
--- easyform/src/components/SignupPage/Signup.jsx
+++ easyform/src/components/SignupPage/Signup.jsx
@@ -1,10 +1,157 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from 'react-router-dom';
+
+import useDebounce from '../../Hooks/useDebounce';
+import { AuthContext } from '../../context/AuthContext';
+
+import {ClipLoader} from 'react-spinners'
 
 const Signup = () => {
+
+    const [email,setEmail]=useState('')
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+    const emailDebounce=useDebounce(email,500)
+    const usernameDebounce=useDebounce(username,500)
+    const passwordDebounce=useDebounce(password,500)
+
+    const {login}=useContext(AuthContext)
+
+    const [loader,setLoader]=useState(false)
+
+
+    const navigate=useNavigate()
+
+
+    const notify=(msg)=> toast(msg)
+
+    const formHandler =async(e)=>{
+        e.preventDefault()
+        
+        setLoader(true)
+        try {
+            
+            
+            if(emailDebounce || passwordDebounce){
+                const response=await axios.post(`http://localhost:4000/register`,{
+                        username:usernameDebounce,
+                        email:emailDebounce,
+                        password:passwordDebounce
+                    })
+                    
+                    if(response){
+                        
+                        // notify('Login successfull')
+                        // setEmail('')
+                        // setPassword('')                        
+                        const token=response.data.token
+                        login(token)
+                        
+                    }
+            }else{
+                notify('Email and Password required')
+                // console.log('email and password required')
+                setEmail('')
+                setPassword('')
+                setLoader(false)
+            }
+
+            
+            
+        } catch (error) {
+            console.log(error.response.data)
+            notify(error.response.data)
+            // console.log(error.response)
+            setEmail('')
+            setPassword('')
+            setLoader(false)
+            
+        }
+
+    }
+
+
+
     return (
-        <div>
-            Signup
-        </div>
+        
+        <>
+            <main className='flex'>
+                <section className='hidden md:block right-section md:w-6/12 h-screen p-5'>
+                    <div className='div-right w-full rounded-md h-90vh flex justify-center items-center '>
+                        
+                        <div className=' w-full '>
+        	                <center>
+                                <div className="div-right-in-text ">
+                                    <h1>Free for everyone</h1>
+                                    <p>Form flow is free for everyone.No hidden charge and it easy to use</p>
+
+                                </div>
+                                <div className="div-right-in-text">
+                                    <h1>Get your website form data at you mail</h1>
+                                    <p>No more intregration of backend for you small business web from .Get your massages directly at your business mail</p>
+                                </div>
+                                <div className="div-right-in-text">
+                                    <h1>All submissions in one place</h1>
+                                    <p>You don't need to go dig down to find your old mails.you can find all your form flow mail in our dashboard </p>
+                                </div>
+                                
+                            </center>
+                        </div>
+                        
+                    </div>
+
+                </section>
+                
+                <section className='left-section w-full md:w-6/12 h-screen '>
+
+                    <div className='flex justify-center items-center h-screen w-full'>
+                        <div className='w-9/12 md:w-7/12 '>
+                        <p className='text-xl md:text-40px text-center mb-6 '>Form Flow</p>
+                            <form action="" onSubmit={formHandler}>
+                               
+                                    <input type="text"
+                                        className='input-feild'
+                                        placeholder='Full name'
+                                        value={username}
+                                        onChange={(e)=>setUsername(e.target.value)}
+
+                                    />
+                                    <br />
+                                    <input type="text"
+                                        className='input-feild'
+                                        placeholder='youremail@email.com'
+                                        value={email}
+                                        onChange={(e)=>setEmail(e.target.value)}
+
+                                    />
+                                    <br />
+                                    <input type="password"
+                                        className='input-feild'
+                                        placeholder='password'
+                                        value={password}
+                                        onChange={(e)=>setPassword(e.target.value)}
+
+                                    />
+                                    <button className='signin-btn'>
+                                        {
+                                            loader ? <ClipLoader size={20} color='white'/> : 'Sign up'
+                                        }
+                                        </button>
+                                    <hr className='mt-2' />
+                                    <Link to='/login'><h1 className='text-left my-2 text-indigo-400'>Allready have an account? sign in</h1></Link>
+                               
+                                
+                            </form>
+                        </div>
+                    </div>
+                    <ToastContainer/>
+                </section>
+            </main>
+        </>
     );
 };
 
